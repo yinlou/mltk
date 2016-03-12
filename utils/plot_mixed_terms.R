6d181593d90444488f2c0db941c32edb9fa4d17e
Sebastien <sdubois.sebastien@gmail.com>
2016-03-12 11:40:44 -0800

    completely rewiewed visualization, now using R and ggplot

---
+++ utils/plot_mixed_terms.R
@@ -0,0 +1,20 @@
+args = commandArgs(trailingOnly=TRUE)
+
+# options(warn=-1, verbose = FALSE)
+
+suppressMessages(library(readr))
+suppressMessages(library(ggplot2))
+suppressMessages(library(tidyr))
+
+filename <- paste(args[1], ".txt", sep = "")
+
+discr_var <- args[2]
+cont_var <- args[3]
+
+df <- read_tsv(filename) %>% gather(state, val, -x)
+
+p <- ggplot(df, mapping = aes(x = x, y = val, color = state)) +
+  geom_point() +
+  geom_line() +
+  labs(x = cont_var, y = "", color = discr_var)
+ggsave(plot = p, filename = paste(args[1], ".png", sep = ""))
