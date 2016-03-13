c9267321b607d685293f12b6aedca04883a3ba15
Sebastien <sdubois.sebastien@gmail.com>
2016-03-12 16:16:59 -0800

    fix typo

--- utils/plot_mixed_terms.R
+++ utils/plot_mixed_terms.R
@@ -11,7 +11,7 @@ filename <- paste(args[1], ".txt", sep = "")
 discr_var <- args[2]
 cont_var <- args[3]
 
-df <- read_tsv(filename) %>% melt(tmp, id.vars = "x", variable.name = "state")
+df <- melt(read_tsv(filename), id.vars = "x", variable.name = "state")
 
 p <- ggplot(df, mapping = aes(x = x, y = value, color = state)) +
   geom_point() +
